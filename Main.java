999be2d67a2febf6a6f9cf8cdd6f05b064cd70a5
Achyut <achyut.bhattarai2025@gmail.com>
2022-04-09 14:13:31 -0500

    Adding assignment 3

--- Main.java
+++ Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("Hello Jeff Maxwell");
         System.out.println("Hello Courtney Andree");
 	System.out.println("Hello Abhishek Bhandari");
+	System.out.println("Hello Achyut Kumar Bhattarai");
     }
 }
