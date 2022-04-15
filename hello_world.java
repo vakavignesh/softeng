f2ce482ef3730402598257f4b2714d0cef16106c
Jake A <skyhy.net@gmail.com>
2022-04-15 12:04:06 -0500

    adding new hello world file with print statement for Jake

---
+++ hello_world.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello Jeff Maxwell");
+        System.out.println("Hello Courtney Andree");
+	    System.out.println("Hello Abhishek Bhandari");
+	    System.out.println("Hello Achyut Kumar Bhattarai");
+        System.out.println("Hello Harshavardhan Gamidi");
+        System.out.println("Hello Penny Willoughby");
+        System.out.println("Hello Naveen Bayanaboina");
+        System.out.println("Bala Bhaskara Bharath Kumar Gottapu");
+        System.out.println("Hello Gangadhar Reddy Muskula");
+        System.out.println("Hello Chandra Kesh Alapati" );
+	System.out.println("Hello Jake O. Adebayo " );
+
+    }
+}
